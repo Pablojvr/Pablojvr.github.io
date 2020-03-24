9fefe7a01a9ab4db2363e09f39309f3f501ff89d
Javierpablo <39129323+Pablojvr@users.noreply.github.com>
2020-03-24 15:36:14 -0600

    Agregando funciones principales

---
+++ core/configAPP.php
@@ -0,0 +1,11 @@
+<?php
+const HOST="localhost";
+ const USER = "root";
+ const PASS="";
+ const DB= "emp4thybdd";
+ const DSN = "mysql:host=".HOST.";dbname=".DB;
+
+
+ const METHOD="AES-256-CBC";
+ const SECRET_KEY='BP@2019';
+ const SECRET_IV='2';
--- core/mainModelo.php
+++ core/mainModelo.php
@@ -4,9 +4,97 @@
 class mainModelo{
 
 
+    protected function conectar(){
+
+        try {
+            $enlace = new PDO(DSN, USER, PASS);
+        } catch (PDOException $e) {
+            echo 'Falló la conexión: ' . $e->getMessage();
+        }
+
+
+        return $enlace;
+    }
+
+    protected function ejecutar_consulta_simple($consulta){
+        $respuesta=self::conectar()->prepare($consulta);
+        $respuesta->execute();
+        return $respuesta;
+    }
+
+    public function encryption($string){
+        $output=FALSE;
+        $key=hash('sha256', SECRET_KEY);
+        $iv=substr(hash('sha256', SECRET_IV), 0, 16);
+        $output=openssl_encrypt($string, METHOD, $key, 0, $iv);
+        $output=base64_encode($output);
+        return $output;
+    }
     
+    protected function decryption($string){
+        $key=hash('sha256', SECRET_KEY);
+        $iv=substr(hash('sha256', SECRET_IV), 0, 16);
+        $output=openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
+        return $output;
+    }
+    
+    protected function generarCodigoAleatorio($letra, $longitud, $num){
+        for($i=1;$i<=$longitud;$i++){
+            $numero=rand(0,9);
+            $letra.=$numero; 
+        }
+        return $letra.$num;
+    }
+
+    protected function limpiarCadena($cadena){
+        $cadena=trim($cadena);
+        $cadena=stripslashes($cadena);
+        $cadena=str_ireplace("<script>","", $cadena);
+        $cadena=str_ireplace("'","", $cadena);
+        $cadena=str_ireplace("</script>","", $cadena);
+        $cadena=str_ireplace("<script src","", $cadena);
+        $cadena=str_ireplace("<script type=","", $cadena);
+        $cadena=str_ireplace("SELECT * FROM","", $cadena);
+        $cadena=str_ireplace("DELETE FROM","", $cadena);
+        $cadena=str_ireplace("INSERT INTO","", $cadena);
+        $cadena=str_ireplace("--","", $cadena);
+        $cadena=str_ireplace("^","", $cadena);
+        return $cadena;
+    }
+
+    protected function sweetAlert($datos){
+        if($datos['Alerta']=="simple"){
+            $alerta="<script>swal(
+                '".$datos['Titulo']."',
+                '".$datos['Texto']."',
+                '".$datos['Tipo']."'
+              ) </script>";
+        }elseif ($datos['Alerta']=="recargar") {
+                        $alerta="<script>swal({
+                            title: '".$datos['Titulo']."',
+                            text: '".$datos['Texto']."',
+                            icon: '".$datos['Tipo']."',
+                            confirmButtonText: 'Aceptar'
+                        }).then( function () {
+                            location.reload();
+                        
+                        })</script>";
+        }elseif ($datos['Alerta']=="limpiar"){
+            $alerta="<script>swal({
+                title: '".$datos['Titulo']."',
+                text: '".$datos['Texto']."',
+                icon: '".$datos['Tipo']."',
+                confirmButtonText: 'Aceptar'
+            }).then( function () {
+                $('.FormularioAjax')[0].reset();
+                location.reload();
+            })</script>";
 
 
+
+        }
+        return $alerta;
+    }
 
 
  }
